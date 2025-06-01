e93199e14a3eeae1c3c80754fa4fe5d6900d7f50
omnaikwadi <omnaikwadi042@gmail.com>
2025-06-01 16:02:54 +0530

    Add files via upload

---
+++ C Programming/Assignment 1/A1/addition of two intigers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void main()
+ {
+    int num1, num2, sum;
+
+    printf("Enter the first integer: ");
+    scanf("%d", &num1);
+
+    printf("Enter the second integer: ");
+    scanf("%d", &num2);
+
+    sum = num1 + num2;
+    printf("The sum of %d and %d is %d\n", num1, num2, sum);
+
+}
